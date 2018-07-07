a70132bc92ea8547f751494c380b7250f66c9a60
rvngrfn <rroberson@wesleyan.edu>
2018-07-07 10:45:10 +0200

    1st homework attempt

---
+++ heromework.js
@@ -0,0 +1,49 @@
+var hero = {
+      name: ""
+      heroic: true
+      inventory =[]
+      health = 
+      weapon = {
+        type: "dagger";
+        damage = 2 
+      }
+    }
+
+    
+
+    var resting = function (hero){
+        var fullHealth = function(hero){
+            object.health = 10
+        }
+        var imageForRest = function(hero){
+            const innUI = document.getElementById('inn') if null{} : 
+        }
+        var clickhealth = function (hero){
+            onclick(fullHealth(hero))
+        }
+    }
+    
+    var pickUpItem = function(hero, weapon){
+        var imageWeapon = function(weapon){
+            const innUI=document.getElementById('dagger')
+        }
+        var storeWeapon = function(weapon){
+            var inventoryLength = hero.inventory.length
+            document.getElementById('dagger').click()
+
+        }
+        
+    }
+
+    var equipWeapon = function (hero){
+        var whoEquips = function (hero){
+            const bagUI = document.getElementById('bag')
+
+        }
+        var emptyInventory = function (hero){
+            const testHero = { weapon: { type: 'sword', damage: 5 }, inventory: [] }
+            const testWeapon = testHero.weapon
+            equipWeapon(testHero)
+        }
+    }
+
